05a0ca08e732e66ad590544ce551e6a6fa79041f
Jain CK <kuriakosejain456@gmail.com>
2025-06-21 19:58:28 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co", // For your placeholder images (if you're using them from here)
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co", // IMPORTANT: This is for your Supabase Storage images
+        port: "",
+        pathname: "/storage/v1/object/public/**", // Adjust pathname if your bucket is private or different structure
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
